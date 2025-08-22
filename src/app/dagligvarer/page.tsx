cfc19c8faf3edd93c34fc4e8cb3b05e6249b156d
Nicolai <w@nicolaivarney.dk>
2025-08-22 10:39:10 +0200

    fix: Add missing closing brace for ternary operator

--- src/app/dagligvarer/page.tsx
+++ src/app/dagligvarer/page.tsx
@@ -655,6 +655,8 @@ export default function DagligvarerPage() {
               ))}
             </div>
 
+            )} {/* Close ternary operator */}
+
             {/* 🚀 FLOATING PRICE HISTORY CARD */}
             {hoveredProduct && (
               <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 pointer-events-none">
